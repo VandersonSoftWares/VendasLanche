7fe3d388337af082c5779fc1375892ff67bfe58e
Vanderson <88204962+VandersonSoftWares@users.noreply.github.com>
2026-08-13 17:26:25 -0300

    Pasta Componentes

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLanche.Repositories.Interfaces;
+
+namespace MVCLanche.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+}
+}
---
+++ Components/_CarrinhoCompraResumo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLanche.Models;
+using MVCLanche.ViewModels;
+using System.Threading.Tasks;
+
+namespace MVCLanche.Components
+{
+    public class CarrinhoCompraResumoViewComponent : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumoViewComponent(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var itens = await Task.Run(() =>
+                _carrinhoCompra.GetCarrinhoCompraItens());
+
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
