32ecaeae2400a7ceb8e4161744f15e184d7ae867
Vanderson <88204962+VandersonSoftWares@users.noreply.github.com>
2026-08-13 17:27:19 -0300

    Add files via upload

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCLanche.Models;
+
+namespace MVCLanche.Context
+{
+    // TEM QUE SER EXPLICITAMENTE PUBLIC
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
+    }
+}
